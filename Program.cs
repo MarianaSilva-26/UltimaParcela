f83b337acd1c3056ebb15ee202c8fabee74588a4
marianaSilva-26 <mariana.silvadossantos.26@gmail.com>
2020-10-05 02:12:04 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System; 
+ 
+
+namespace UltimaParcela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal financiamento; 
+            decimal parcelas; 
+            DateTime UltimaParcela;
+            decimal resultadoFinal; 
+
+            DateTime dataAtual = DateTime.Now; 
+            UltimaParcela = dataAtual.AddMonths(8);
+
+            Console.Write("valor total do financiamento..:");
+            financiamento = decimal.Parse(Console.ReadLine());
+
+            Console.Write("quantidade de parcelas mensais (sem juros) ..:");
+            parcelas = decimal.Parse(Console.ReadLine()); 
+            Console.Write("\n"); 
+
+            resultadoFinal = financiamento / parcelas; 
+            
+            Console.WriteLine($"Sua ultima parcela será em : {UltimaParcela}, no valor de r${resultadoFinal}");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # UltimaParcela
-Receba o valor total a ser pago e a quantidade de parcelas mensais de um financiamento. Exiba a data e o valor da última parcela.
+Receba o valor total a ser pago e a quantidade de parcelas mensais de um financiamento. 
+
+Exiba a data e o valor da última parcela.
